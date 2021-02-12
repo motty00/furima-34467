26ee64a03b33e9e093f21c38321e4c8314babf9a
motty00 <kowappa1104@yahoo.co.jp>
2021-02-12 15:30:45 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :encrypted_password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  validates :name,               presence: true
+  validates :last_name,          presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+  validates :first_name,         presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+  validates :last_name_kana,     presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana,    presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birth_date,         presence: true
+
 end
