2d8d08f1764343e809e2f73a076951e96aa4576b
motty00 <kowappa1104@yahoo.co.jp>
2021-02-13 15:08:33 +0900

    インデントの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,15 @@ class User < ApplicationRecord
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   
   with_options presence: true do
-  validates :name
-  with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
-  validates :last_name
-  validates :first_name
-  end
-  with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
-  validates :last_name_kana
-  validates :first_name_kana
-  end
-  validates :birth_date
+    validates :name
+    validates :birth_date
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+      validates :last_name_kana
+      validates :first_name_kana
+   end
   end
 end
