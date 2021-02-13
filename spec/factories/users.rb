da924d278294849a31160011fe8985f140d5a4e6
motty00 <kowappa1104@yahoo.co.jp>
2021-02-13 10:38:39 +0900

    FactoryBotの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,8 +4,8 @@ FactoryBot.define do
     password              {"hoge123"}
     password_confirmation {password}
     name                  {Faker::Name.initials(number: 5)}
-    last_name             {Faker::Name.initials(number: 2)}
-    first_name            {Faker::Name.initials(number: 2)}
+    last_name             {Gimei.last.kanji}
+    first_name            {Gimei.first.kanji}
     last_name_kana        {Gimei.last.katakana }
     first_name_kana       {Gimei.first.katakana }
     birth_date            {Faker::Date.in_date_period}
