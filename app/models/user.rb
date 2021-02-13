fdbd80ed7c68c81a5258a422f34f3d0cc9c1d355
motty00 <kowappa1104@yahoo.co.jp>
2021-02-13 14:22:27 +0900

    共通のバリデーションの統合

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-  validates :name,               presence: true
-  validates :last_name,          presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
-  validates :first_name,         presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
-  validates :last_name_kana,     presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana,    presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birth_date,         presence: true
+  
+  with_options presence: true do
+  validates :name
+  with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
+  validates :last_name
+  validates :first_name
+  end
+  with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+  validates :last_name_kana
+  validates :first_name_kana
+  end
+  validates :birth_date
+  end
 end
