57bc89b62848f27cac3be644c703d1c9d2a1b3b8
motty00 <kowappa1104@yahoo.co.jp>
2021-02-14 12:36:20 +0900

    アソシエーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,17 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
 
-belongs_to :user
-has_one    :order
 
-has_one_attached :image
+
+  belongs_to :user
+  has_one    :order
+
+  has_one_attached :image
 
 
 end
