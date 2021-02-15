21613f237fd31371986bf76deae1ca21e139ccc6
motty00 <kowappa1104@yahoo.co.jp>
2021-02-15 14:22:02 +0900

    priceのバリデーションに半角数字のみ許可の記述を追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,7 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :info
-    validates :price, inclusion: { in: 300..9_999_999 }
+    validates :price, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
   end
   with_options numericality: { other_than: 1 } do
     validates :category_id
