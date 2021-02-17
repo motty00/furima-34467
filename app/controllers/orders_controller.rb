dc1bd4f86909b7d8023bd2e1f2a9f1200dc1b167
motty00 <kowappa1104@yahoo.co.jp>
2021-02-17 20:07:57 +0900

    エラーメッセージを表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,8 +2,8 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new 
   end
-
 
   def create
     @item = Item.find(params[:item_id])
