de01845ba789e999243498d23adce42b4b7ae89d
motty00 <kowappa1104@yahoo.co.jp>
2021-02-17 19:54:31 +0900

    購入情報、住所情報をテーブルに保存

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,26 @@
 class OrdersController < ApplicationController
 
   def index
-    @order = ItemAddress.new
     @item = Item.find(params[:item_id])
   end
 
+
   def create
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(address_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+
+  private
+
+  def address_params
+    params.permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number, :order_id, :user_id, :item_id).merge(user_id: current_user.id)
   end
 
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,4 +1,4 @@
-class ItemAddress
+class OrderAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :order_id, :user_id, :item_id
 
@@ -12,7 +12,7 @@ class ItemAddress
 
 
   def save
-    Orders.create(user_id: user_id, item_id: item_id)
-    Addresses.create(postal_code: postal_code, prefecture: prefecture, city: city, address: address, building: building, phone_number: phone_number, order_id: order.id)
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, order_id: order.id)
   end
 end
