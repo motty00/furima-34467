ff615ea7d82bb65df5db21adad4d821caa2b8142
motty00 <kowappa1104@yahoo.co.jp>
2021-02-18 12:04:01 +0900

    FactoryBotの作成

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :address do
-    
+    postal_code            { "123-4567" }
+    prefecture_id          { Faker::Number.between(from: 2, to: 48) }
+    city                   { Gimei.city.kanji }
+    address                { "青山1-1-1" }
+    building               { "柳ビル103" }
+    phone_number           { Faker::Number.leading_zero_number(digits: 11) }
+    association :order
   end
 end
