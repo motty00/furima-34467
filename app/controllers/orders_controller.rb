3b90a9a5516f128093a41c8814670dc56f87e2ea
motty00 <kowappa1104@yahoo.co.jp>
2021-02-18 21:13:05 +0900

    Rubocopを実行

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,6 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
 
-
   def index
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
@@ -19,20 +18,20 @@ class OrdersController < ApplicationController
     end
   end
 
-
   private
 
   def address_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number, :order_id).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number, :order_id).merge(
+      user_id: current_user.id, item_id: @item.id, token: params[:token]
+    )
   end
 
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
       amount: @item.price,
       card: address_params[:token],
       currency: 'jpy'
     )
   end
-
 end
--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,13 +1,12 @@
 FactoryBot.define do
   factory :order_address do
-    postal_code             { "123-1234" }
+    postal_code             { '123-1234' }
     prefecture_id           { Faker::Number.between(from: 2, to: 48) }
     city                    { Gimei.city.kanji }
-    address                 { "1542-3" }
-    building                { "柳ビル103" }
+    address                 { '1542-3' }
+    building                { '柳ビル103' }
     phone_number            { Faker::Number.leading_zero_number(digits: 11) }
 
-    token {"tok_abcdefghijk00000000000000000"}
+    token { 'tok_abcdefghijk00000000000000000' }
   end
 end
-
