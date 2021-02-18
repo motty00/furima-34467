485289b5ebb78091da7bcfef085c103f7ae8651b
motty00 <kowappa1104@yahoo.co.jp>
2021-02-18 13:47:04 +0900

    FactoryBotの修正

---
+++ spec/models/order_address_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+RSpec.describe OrderAddress, type: :model do
+  before do
+    @address = FactoryBot.build(:order_address)
+  end
+  describe '購入情報登録' do
+    context '購入情報が登録できるとき' do
+      it 'postal_code、prefecture_id、city、address、phone_numberが存在すれば登録できる' do
+      end
+      it 'postal_codeには「-]ハイフンが存在すれば登録できる' do
+      end
+      it 'buildingは存在しなくても登録できる' do
+      end
+      it 'phone_numberは11桁以内の数字であれば登録できる' do
+      end
+    end
+
+
+
+
+    context '購入情報が登録できないとき' do
+      it 'postal_codeが空だと登録できない' do
+      end
+      it 'postal_codeに「-」ハイフンが存在しないと登録できない' do
+      end
+      it 'prefecture_idが「１」だと登録できない' do
+      end
+      it 'cityが空だと登録できない' do
+      end
+      it 'addressが空だと登録できない' do
+      end
+      it 'phone_numberが空では登録できない' do
+      end
+      it 'phone_numberが数字以外だと登録できない' do
+      end
+      it 'phone_numberが12桁以上だと登録できない' do
+      end
+    end
+  end
+
+
+end
+
+
+
