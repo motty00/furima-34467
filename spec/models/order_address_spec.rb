1c1e6b927500f9e6bb606a661686c69abf25dc5b
motty00 <kowappa1104@yahoo.co.jp>
2021-02-18 14:19:36 +0900

    購入情報を登録できない場合のテストコード

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -23,29 +23,44 @@ RSpec.describe OrderAddress, type: :model do
     end
 
 
-
-
     context '購入情報が登録できないとき' do
       it 'postal_codeが空だと登録できない' do
+        @address.postal_code = ""
+        @address.valid?
+        expect(@address.errors.full_messages).to include("Postal code can't be blank", "Postal code is invalid")
       end
       it 'postal_codeに「-」ハイフンが存在しないと登録できない' do
+        @address.postal_code = "1231234"
+        @address.valid?
+        expect(@address.errors.full_messages).to include("Postal code is invalid")
       end
       it 'prefecture_idが「１」だと登録できない' do
+        @address.prefecture_id = 1
+        @address.valid?
+        expect(@address.errors.full_messages).to include("Prefecture must be other than 1")
       end
       it 'cityが空だと登録できない' do
+        @address.city = ""
+        @address.valid?
+        expect(@address.errors.full_messages).to include("City can't be blank")
       end
       it 'addressが空だと登録できない' do
+        @address.address = ""
+        @address.valid?
+        expect(@address.errors.full_messages).to include("Address can't be blank")
       end
       it 'phone_numberが空では登録できない' do
+        @address.phone_number = ""
+        @address.valid?
+        expect(@address.errors.full_messages).to include("Phone number can't be blank")
       end
       it 'phone_numberが数字以外だと登録できない' do
-      end
-      it 'phone_numberが12桁以上だと登録できない' do
+        @address.phone_number = "aaaaaaaaaa"
+        @address.valid?
+        expect(@address.errors.full_messages).to include("Phone number is not a number")
       end
     end
   end
-
-
 end
 
 
