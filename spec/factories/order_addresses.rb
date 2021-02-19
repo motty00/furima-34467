99af70518cef719c4f9e450c8e16a2d5088695bc
motty00 <kowappa1104@yahoo.co.jp>
2021-02-19 11:42:34 +0900

    factorybotでuser_idとitem_idを生成

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -7,6 +7,8 @@ FactoryBot.define do
     building                { '柳ビル103' }
     phone_number            { Faker::Number.leading_zero_number(digits: 11) }
 
-    token { 'tok_abcdefghijk00000000000000000' }
+    token                   { 'tok_abcdefghijk00000000000000000' }
+    user_id                 { '2' }
+    item_id                 { '3' }
   end
 end
